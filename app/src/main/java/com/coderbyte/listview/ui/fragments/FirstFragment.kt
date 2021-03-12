212c41d70f888c1a89948293915534ae70ea1db2
mgangulo <gaby9415@hotmail.com>
2021-03-11 20:16:20 -0400

    Error handling

--- app/src/main/java/com/coderbyte/listview/ui/fragments/FirstFragment.kt
+++ app/src/main/java/com/coderbyte/listview/ui/fragments/FirstFragment.kt
@@ -1,11 +1,13 @@
 package com.coderbyte.listview.ui.fragments
 
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.fragment.findNavController
 import com.coderbyte.listview.R
 import com.coderbyte.listview.databinding.FragmentFirstBinding
@@ -59,11 +61,30 @@ class FirstFragment : Fragment(),FirstFragmentPresenter.UserPresenter {
 
     override fun onFailure(msg: String?) {
         if (context!=null) {
+            showAlertDialog(when (msg==null){
+                true -> getString(R.string.unknown_error)
+                else -> msg
+            })
         }
     }
 
     override fun onFailure(code: Int) {
         if (context!=null) {
+            showAlertDialog(getString(R.string.error_code,code))
         }
+    }
+
+    fun showAlertDialog(msg:String){
+        val builder = AlertDialog.Builder(requireContext())
+
+        builder.setMessage(msg)
+            .setTitle(R.string.error)
+            .setPositiveButton(R.string.ok,object : DialogInterface.OnClickListener{
+                override fun onClick(dialog: DialogInterface?, which: Int) {
+                    dialog?.dismiss();
+                }
+            })
+
+        builder.create().show()
     }
 }
